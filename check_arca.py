d763211b91ea518705897c2f736e2c0bdb60ebd5
pakpakyee123 <gimm25259481@gmail.com>
2026-05-30 20:51:41 +0900

    Update check_arca.py

--- check_arca.py
+++ check_arca.py
@@ -1,10 +1,11 @@
 import requests
 
-url = "https://arca.live/u/@하루/86785959"
+url = "https://arca.live/b/commission"
 
 r = requests.get(
     url,
     headers={"User-Agent": "Mozilla/5.0"}
 )
 
-print(r.text[:5000])
+print(r.status_code)
+print(r.text[:3000])
